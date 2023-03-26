b0400553b1928f6a4fa0154427946ff11adb11b6
alimoqimi97 <41248132+alimoqimi97@users.noreply.github.com>
2023-03-27 02:26:30 +0430

    Implement chatbox navbar

--- src/Components/Chatbox/index.tsx
+++ src/Components/Chatbox/index.tsx
@@ -1,13 +1,23 @@
-import React, { FC } from 'react';
-import styles from './styles.module.scss';
+import { FC } from "react";
+import Image from "next/image";
+import { BsFillCameraVideoFill } from "react-icons/bs";
+import { AiOutlinePlus } from "react-icons/ai";
+import { FiMoreHorizontal } from "react-icons/fi";
+import styles from "./styles.module.scss";
 
-export interface ChatboxProps {
-}
+export interface ChatboxProps {}
 
-export const Chatbox: FC<ChatboxProps> = (props) => {
+export const Chatbox: FC<ChatboxProps> = props => {
     return (
         <div className={styles.Chatbox} data-testid="Chatbox">
-            Chatbox
+            <div className={styles.chatInfo}>
+                <span>Jane</span>
+                <div className={styles.chatIcons}>
+                    <BsFillCameraVideoFill />
+                    <AiOutlinePlus />
+                    <FiMoreHorizontal />
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import { Chats, Navbar, Search } from "..";
 import styles from "./styles.module.scss";
 
